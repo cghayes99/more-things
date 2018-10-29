3078492a71561c9696941e4e1f3b24fed67ee3b0
Chris <chris@brogard.io>
2018-10-29 17:01:58 -0500

    crap

---
+++ final-alpha/wwwroot/Metrics/product-count/figure.js
@@ -0,0 +1,174 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Product Count by Category", 
+        "paper_bgcolor": "#FFFFFF", 
+        "plot_bgcolor": "rgb(255, 255, 255)", 
+        "yaxis": {
+            "tickfont": {
+                "color": "#666666"
+            }, 
+            "automargin": false, 
+            "title": "Number of Advertised Products", 
+            "autorange": true, 
+            "zerolinecolor": "#F6F6F6", 
+            "range": [
+                0, 
+                39313.68421052631
+            ], 
+            "gridcolor": "#F6F6F6", 
+            "titlefont": {
+                "color": "#666666", 
+                "size": 16
+            }, 
+            "type": "linear", 
+            "side": "left"
+        }, 
+        "colorway": [
+            "#1f77b4", 
+            "#ff7f0e", 
+            "#2ca02c", 
+            "#d62728", 
+            "#9467bd", 
+            "#8c564b", 
+            "#e377c2", 
+            "#7f7f7f", 
+            "#bcbd22", 
+            "#17becf"
+        ], 
+        "titlefont": {
+            "color": "#151516", 
+            "size": 18
+        }, 
+        "xaxis": {
+            "tickfont": {
+                "color": "#666666"
+            }, 
+            "automargin": true, 
+            "showticklabels": true, 
+            "tickmode": "linear", 
+            "title": "Category", 
+            "tick0": 7, 
+            "tickangle": 45, 
+            "ticks": "", 
+            "dtick": 1, 
+            "showgrid": false, 
+            "zerolinecolor": "#F6F6F6", 
+            "range": [
+                -0.5, 
+                35.5
+            ], 
+            "gridcolor": "#F6F6F6", 
+            "zeroline": false, 
+            "titlefont": {
+                "color": "#666666", 
+                "size": 16
+            }, 
+            "showspikes": false, 
+            "type": "category", 
+            "autorange": true, 
+            "side": "bottom"
+        }, 
+        "hovermode": "x", 
+        "bargroupgap": 0, 
+        "legend": {
+            "bgcolor": "#FFFFFF", 
+            "font": {
+                "color": "#666666"
+            }
+        }
+    }, 
+    "data": [
+        {
+            // "name": null, 
+            // "ysrc": "hollymcgilly:1:07f531", 
+            // "xsrc": "hollymcgilly:1:f31a72", 
+            "marker": {
+                "color": "rgba(104, 112, 246, 0.6)", 
+                "line": {
+                    "color": "rgb(88, 91, 206)", 
+                    "width": 1
+                }
+            }, 
+            "y": [
+                "37348", 
+                "3565", 
+                "1247", 
+                "1029", 
+                "256", 
+                "353", 
+                "101", 
+                "1012", 
+                "943", 
+                "3308", 
+                "1950", 
+                "1167", 
+                "489", 
+                "428", 
+                "7790", 
+                "1511", 
+                "3725", 
+                "20205", 
+                "1037", 
+                "1734", 
+                "154", 
+                "4902", 
+                "433", 
+                "531", 
+                "722", 
+                "1217", 
+                "483", 
+                "3491", 
+                "1349", 
+                "151", 
+                "10722", 
+                "2285", 
+                "7165", 
+                "9349", 
+                "801", 
+                "5919"
+            ], 
+            "x": [
+                "Apparel", 
+                "Appliances", 
+                "Arts & Crafts", 
+                "Automobile", 
+                "Baby", 
+                "Books", 
+                "Camcorders", 
+                "Cameras", 
+                "Cellphones", 
+                "Computer Accessories", 
+                "Computer Components", 
+                "Computers", 
+                "Desktops", 
+                "Digital Cameras", 
+                "Electronics", 
+                "Food", 
+                "Holiday Items", 
+                "Home Items", 
+                "Home Theatre", 
+                "Kitchen", 
+                "Misc", 
+                "Movies & TV", 
+                "Music", 
+                "Musical Instruments", 
+                "Office", 
+                "Outdoor", 
+                "Patio & Garden", 
+                "Personal Care", 
+                "Pet", 
+                "Promotional Items", 
+                "Sporting Goods", 
+                "Televisions", 
+                "Tools", 
+                "Toys", 
+                "Travel", 
+                "Video Games"
+            ], 
+            "type": "bar", 
+            "orientation": "v"
+        }
+    ]
+}
